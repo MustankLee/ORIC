38a2f98a78bde90932a1cd8ff6b63ae2bcfc65ce
unknown <ricosusanto777@gmail.com>
2023-01-01 23:05:53 +0700

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <>
       <Router>
         <div className="flex max-md:block">
-          <Navbar />
+          <Navbar />  
           <Home />
           <div className="h-screen w-5/6 overflow-y-auto px-6 py-4 bg-white max-md:w-full">
             <Routes>
